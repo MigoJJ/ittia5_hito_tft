b8550220f90aa5915762ea9e8acde7b3043b01fc
Woon Jung Koh <jjkoh21@gmail.com>
2023-03-01 22:31:31 +0900

    123

--- src/ThyroidSource/MyFrame.java
+++ src/ThyroidSource/MyFrame.java
@@ -3,7 +3,6 @@ package ThyroidSource;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
